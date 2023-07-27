e73189f97d2fca12e36f5f92e9a80dacb8513272
PasyaMulfi <pasyamulfi@gmail.com>
2023-07-27 11:04:12 +0700

    INI VERCEL UPDATE

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -18,9 +18,10 @@ import { TheCard } from 'flowbite-vue'
         <the-card variant="image" img-src="tskids.jpg" img-alt="Desk">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Fear of God Essentials Kids T-Shirt Amber</h5>
         <p class="font-normal text-gray-700 dark:text-gray-400">
-          Please Make Sure The Size Fits You.
           Authentic. Guaranteed.
+          We thoroughly check every purchase you make and applies our company's 
         </p>
+           <p class="ml-12 font-bold text-lime-700">IDR 1.850.000</p>
       </the-card></div>
       
       <div class="mx-8 my-9"> <the-card variant="image" img-src="jordanretro.jpg" img-alt="Desk">
@@ -28,6 +29,7 @@ import { TheCard } from 'flowbite-vue'
         <p class="font-normal text-gray-700 dark:text-gray-400">
           Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
         </p>
+           <p class="ml-12 font-bold text-lime-700">IDR 2.350.000</p>
       </the-card>
     </div>
     <div class="mx-8 my-8"> <the-card variant="image" img-src="crocsclassic.jpg" img-alt="Desk">
@@ -36,6 +38,7 @@ import { TheCard } from 'flowbite-vue'
           Please Make Sure The Size Fits You.
           Authentic. Guaranteed.
         </p>
+           <p class="ml-12 font-bold text-lime-700">IDR 2.900.000</p>
       </the-card>
     </div>
     <div class="mx-8 my-8">
@@ -44,7 +47,9 @@ import { TheCard } from 'flowbite-vue'
         <p class="font-normal text-gray-700 dark:text-gray-400">
             Please Make Sure The Size Fits You.
               Authentic. Guaranteed.
+              We thoroughly check every purchase you make and applies our company's
         </p>
+           <p class="ml-12 font-bold text-lime-700">IDR 1.650.000</p>
       </the-card>
     </div>
 </div>
@@ -58,7 +63,9 @@ import { TheCard } from 'flowbite-vue'
             <p class="font-normal text-gray-700 dark:text-gray-400">
                Please Make Sure The Size Fits You.
                   Authentic. Guaranteed.
+                  We thoroughly check every purchase you make and applies our company
             </p>
+               <p class="ml-12 font-bold text-lime-700">IDR 600.000</p>
           </the-card>
           </div>
           <div class="mx-8 my-8">
@@ -67,7 +74,9 @@ import { TheCard } from 'flowbite-vue'
                 <p class="font-normal text-gray-700 dark:text-gray-400">
                     Please Make Sure The Size Fits You.
                       Authentic. Guaranteed.
+                      We thoroughly check every purchase you make and applies our company's guarantee to the product's legitimacy     
                 </p>
+                   <p class="ml-12 font-bold text-lime-700">IDR 900.000</p>
               </the-card>
           </div>
           <div class="mx-8 my-8">
@@ -77,6 +86,7 @@ import { TheCard } from 'flowbite-vue'
                  Please Make Sure The Size Fits You.
                  Authentic. Guaranteed.
                 </p>
+                   <p class="ml-12 font-bold text-lime-700">IDR 2.000.000</p>
               </the-card>
               </div>
               <div class="mx-8 my-8">
@@ -85,7 +95,9 @@ import { TheCard } from 'flowbite-vue'
                 <p class="font-normal text-gray-700 dark:text-gray-400">
                   Please Make Sure The Size Fits You.
                    Authentic. Guaranteed.
+                   We thoroughly check every purchase you make and applies our company's guarante
                 </p>
+                   <p class="ml-12 font-bold text-lime-700">IDR 800.000</p>
               </the-card>
               </div>
      </div>
@@ -100,6 +112,7 @@ import { TheCard } from 'flowbite-vue'
                     <p class="font-normal text-gray-700 dark:text-gray-400">
                       Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
                     </p>
+                       <p class="ml-12 font-bold text-lime-700">IDR 1.650.000</p>
                   </the-card>
                   </div>
                    <div class="mx-8 my-8">
@@ -108,14 +121,17 @@ import { TheCard } from 'flowbite-vue'
                     <p class="font-normal text-gray-700 dark:text-gray-400">
                       Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
                     </p>
+                    <p class="ml-12 font-bold text-lime-700">IDR 2.200.000</p>
                   </the-card>
+                  
                   </div>
                    <div class="mx-8 my-8">
                  <the-card variant="image" img-src="spiderman.jpg" img-alt="Desk">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Air Jordan 1 High OG Spider-Man Across the Spider-Verse</h5>
                     <p class="font-normal text-gray-700 dark:text-gray-400">
-                      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                      Here are the biggest enter
                     </p>
+                       <p class="ml-12 font-bold text-lime-700">IDR 3.700.000</p>
                   </the-card>
                   </div>
                    <div class="mx-8 my-8">
@@ -124,6 +140,7 @@ import { TheCard } from 'flowbite-vue'
                     <p class="font-normal text-gray-700 dark:text-gray-400">
                       Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
                     </p>
+                       <p class="ml-12 font-bold text-lime-700">IDR 17.000.000</p>
                   </the-card>
                   </div>        
       </div>
--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,7 +5,7 @@
           <div class="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
                   <img src="kickavenue.jpg" class="h-8 mr-3" alt="FlowBite Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">kickavenue</span>
+                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kickavenue</span>
               </a>
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -13,10 +13,10 @@
                   <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a href="https://flowbite.com/" class="hover:underline">kickavenue</a>
+                          <a href="https://flowbite.com/" class="hover:underline">Kickavenue</a>
                       </li>
                       <li>
-                          <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                          <a href="https://tailwindcss.com/" class="hover:underline">FAQ</a>
                       </li>
                   </ul>
               </div>
@@ -24,10 +24,10 @@
                   <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
+                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Payment Confirmation</a>
                       </li>
                       <li>
-                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
+                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Buying & Selling Guide</a>
                       </li>
                   </ul>
               </div>
